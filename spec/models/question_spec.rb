26d3403d6074d6d10c3d4cbab2c626bf390e0cd0
Roman Perminov <permidon@me.com>
2017-02-07 10:22:03 -0500

    Question title and body validation

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates presents of title' do
+    expect(Question.new(body: '123')).to_not be_valid
+  end
+
+  it 'validates presents of body' do
+    expect(Question.new(title: '123')).to_not be_valid
+  end
 end
