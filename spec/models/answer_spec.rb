a2d40ebb384effabcf3ead61ea09fce5f66983a3
Roman Perminov <permidon@me.com>
2017-02-07 13:52:47 -0500

    Answer dependent test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should belong_to :question }
+  it { should belong_to(:question).dependent(:destroy) }
 
   it { should validate_presence_of :body }
 end
