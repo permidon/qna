5acf8e71ee6a699e8051e3d2cd5228e5a167a3ce
Roman Perminov <permidon@me.com>
2017-02-10 19:42:52 -0500

    Answer to Q acceptance test

---
+++ spec/acceptance/create_answer_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Create answer', %q{
+  In order to give my advice
+  As an authenticated user
+  I want to be able to answer to questions
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:answer) { create(:answer) }
+
+  scenario 'Authenticated user creates a answer' do
+    sign_in(user)
+
+    visit question_path(question)
+    fill_in 'Answer', with: answer.body
+    click_on 'Create'
+
+    expect(page).to have_content answer.body
+  end
+
+  scenario 'Non-authenticated user creates a answer' do
+    visit question_path(question)
+    fill_in 'Answer', with: answer.body
+    click_on 'Create'
+
+    save_and_open_page
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
