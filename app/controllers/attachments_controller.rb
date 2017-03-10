8c49062f1efbdc1cb95406963df4dea78b1981be
Roman Perminov <permidon@me.com>
2017-03-10 17:53:03 -0500

    Thin attachments controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,15 +1,19 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_attachment
+
+  respond_to :js
 
   def destroy
+    if current_user.author_of?(@attachable)
+      respond_with(@attachment.destroy)
+    end
+  end
+
+  private
+
+  def set_attachment
     @attachment = Attachment.find(params[:id])
     @attachable = @attachment.attachable
-
-    if current_user.author_of?(@attachable)
-      @attachment.destroy
-      flash[:notice] ='The attachment has been successfully deleted.'
-    else
-      flash[:alert] ='You can not delete this attachment.'
-    end
   end
 end
