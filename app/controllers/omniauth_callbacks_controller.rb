8c4d56892e5b9e5f6e2a3f42e3d9876240c20c9d
Roman Perminov <permidon@me.com>
2017-03-19 12:34:28 -0400

    Use devise session section to store data

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,22 +8,20 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def confirm_email
-    session[:provider_data]['unconfirm'] = 'true'
-    session[:provider_data]['info'] = OmniAuth::AuthHash.new(params[:auth])
-    @omni_auth = session[:provider_data]
+    session['devise.provider_data']['unconfirm'] = 'true'
+    session['devise.provider_data']['info'] = OmniAuth::AuthHash.new(params[:auth])
     omni_auth_actions
   end
 
   protected
 
   def omni_auth_actions
-    @omni_auth ||= request.env['omniauth.auth'].except('extra')
-    session[:provider_data] = @omni_auth
+    session['devise.provider_data'] ||= request.env['omniauth.auth'].except('extra')
+    @omni_auth = session['devise.provider_data']
     @user = User.find_for_oauth(@omni_auth)
     if @user && @user.persisted? && @user.confirmed?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message :notice, :success, kind: @omni_auth['provider'].capitalize if is_navigational_format?
-      session[:provider_data] = nil
     end
     if @user && !@user.confirmed?
       render 'devise/confirmations/new'
