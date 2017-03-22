05856822a5dfd03fa857fa70ebdd8ae12bd93d57
Roman Perminov <permidon@me.com>
2017-03-22 13:56:01 -0400

    APIv1 GET /profiles/all_but_me

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     respond_with current_resource_owner
   end
+
+  def all_but_me
+    respond_with User.where.not(id: current_resource_owner)
+  end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -25,16 +25,60 @@ describe 'Profile API' do
       end
 
       %w(id email created_at updated_at admin).each do |attr|
-        it "contains #{attr}" do
+        it "contains current user data - #{attr}" do
           expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
         end
       end
 
       %w(password encrypted_password).each do |attr|
-        it "does not contain #{attr}" do
+        it "does not contain current user data - #{attr}" do
           expect(response.body).to_not have_json_path(attr)
         end
       end
+    end
+  end
+
+  describe 'GET /all_but_me' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/all_but_me', params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles/all_but_me', params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let(:access_token) { create(:access_token, resource_owner_id: users[2].id) }
+
+      before { get '/api/v1/profiles/all_but_me', params: { format: :json, access_token: access_token.token } }
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      %w(id email created_at updated_at admin).each do |attr|
+        it "contains other users data - #{attr}" do
+          expect(response.body).to be_json_eql(users[0].send(attr.to_sym).to_json).at_path("0/#{attr}")
+          expect(response.body).to be_json_eql(users[1].send(attr.to_sym).to_json).at_path("1/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain other users data - #{attr}" do
+          expect(response.body).to_not have_json_path("0/#{attr}")
+          expect(response.body).to_not have_json_path("1/#{attr}")
+        end
+      end
+
+      it "does not contain current user data" do
+        expect(response.body).to_not have_json_path("2")
+      end
+
     end
   end
 end
