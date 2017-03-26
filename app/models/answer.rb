f7c2ed00d07b552234599042ea368400820dbca8
Roman Perminov <permidon@me.com>
2017-03-26 15:29:33 -0400

    Publish answer via Model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,6 +8,8 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
+  after_create :publish_answer
+
   scope :helpful, -> { order(best: :desc).order(created_at: :asc) }
 
   def set_best_status
@@ -15,5 +17,25 @@ class Answer < ApplicationRecord
       self.question.answers.update_all(best: false)
       self.update!(best: true)
     end
+  end
+
+  def publish_answer
+    return if self.errors.any?
+
+    attachments = []
+    self.attachments.each do |a|
+      attachment = {}
+      attachment[:id] = a.id
+      attachment[:url] = a.file.url
+      attachment[:name] = a.file.identifier
+      attachments << attachment
+    end
+
+    ActionCable.server.broadcast(
+        "answers-question-#{self.question.id}",
+        answer: self,
+        attachments: attachments,
+        question: self.question
+    )
   end
 end
