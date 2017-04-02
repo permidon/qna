6d8dde77e5b88cc61716bf8c4b742bc00d6f6d85
Roman Perminov <permidon@me.com>
2017-04-02 18:57:18 -0400

    Easy access to subscribers of the question

--- app/jobs/question_subscribe_job.rb
+++ app/jobs/question_subscribe_job.rb
@@ -2,8 +2,9 @@ class QuestionSubscribeJob < ApplicationJob
   queue_as :default
 
   def perform(answer)
-    Subscription.where(question_id: answer.question_id).each do |subscription|
-      SubscriptionMailer.new_answer(subscription.user, answer).deliver_later
+    # Subscription.where(question_id: answer.question_id).each do |subscription|
+    answer.question.subscribers.each do |subscripber|
+      SubscriptionMailer.new_answer(subscripber, answer).deliver_later
     end
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
   has_many :subscriptions, dependent: :destroy
+  has_many :subscribers, through: :subscriptions, source: :user
 
   validates :title, :body, :user_id, presence: true
 
