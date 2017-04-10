aee08d1a9b5e736eab6efb953b020f80dc895004
Roman Perminov <permidon@me.com>
2017-04-09 23:12:02 -0400

    cap config

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+# config valid only for current version of Capistrano
+lock "3.8.0"
+
+set :application, 'qna'
+set :repo_url, 'git@github.com:permidon/qna.git'
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml .env}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+set :default_shell, '/bin/bash -l'
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
